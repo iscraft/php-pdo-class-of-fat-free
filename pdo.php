f827dec815a803625de1b77cc3f251ebc80bcff0
iscraft <moaphi@gmail.com>
2017-04-07 11:47:40 +0800

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,31 +1,10 @@
 <?php
 
 /*
-
-	Copyright (c) 2009-2016 F3::Factory/Bong Cosca, All rights reserved.
-
-	This file is part of the Fat-Free Framework (http://fatfreeframework.com).
-
-	This is free software: you can redistribute it and/or modify it under the
-	terms of the GNU General Public License as published by the Free Software
-	Foundation, either version 3 of the License, or later.
-
-	Fat-Free Framework is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	General Public License for more details.
-
-	You should have received a copy of the GNU General Public License along
-	with Fat-Free Framework.  If not, see <http://www.gnu.org/licenses/>.
-
+	@license	GNU General Public License http://www.gnu.org/licenses
 */
-
 //namespace DB;
-
 //! PDO wrapper
-
-
-
 /*******************/
 class SQL {
 
