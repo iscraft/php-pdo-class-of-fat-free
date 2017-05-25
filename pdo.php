a7919a36f27ef03f6094540437802cad7d23a842
iscraft <moaphi@gmail.com>
2017-05-25 14:48:23 +0800

    add one&column query

--- pdo.php
+++ pdo.php
@@ -1,35 +1,24 @@
 <?php
-
 /*
-
 	Copyright (c) 2009-2017 F3::Factory/Bong Cosca, All rights reserved.
-
 	This file is part of the Fat-Free Framework (http://fatfreeframework.com).
-
 	This is free software: you can redistribute it and/or modify it under the
 	terms of the GNU General Public License as published by the Free Software
 	Foundation, either version 3 of the License, or later.
-
 	Fat-Free Framework is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 	General Public License for more details.
-
 	You should have received a copy of the GNU General Public License along
 	with Fat-Free Framework.  If not, see <http://www.gnu.org/licenses/>.
-
 */
-
 //namespace DB;
-
 //! PDO wrapper
 class SQL {
-
 	//@{ Error messages
 	const
 		E_PKey='Table %s does not have a primary key';
 	//@}
-
 	const
 		PARAM_FLOAT='float';
 	//custom add start
@@ -61,7 +50,6 @@ function hash($str) {
 function lastInsertId(){
 	return $this->pdo->lastInsertId();
 }
-
 function stringify($arg,array $stack=NULL) {
 	if ($stack) {
 		foreach ($stack as $node)
@@ -92,7 +80,6 @@ function stringify($arg,array $stack=NULL) {
 			return var_export($arg,TRUE);
 	}
 }
-
 function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 	$tag='';
 	if (is_array($ttl))
@@ -228,6 +215,278 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 		$this->commit();
 	return $result;
 }
+//get one
+function one($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
+	$tag='';
+	if (is_array($ttl))
+		list($ttl,$tag)=$ttl;
+	$auto=FALSE;
+	if (is_null($args))
+		$args=[];
+	elseif (is_scalar($args))
+		$args=[1=>$args];
+	if (is_array($cmds)) {
+		if (count($args)<($count=count($cmds)))
+			// Apply arguments to SQL commands
+			$args=array_fill(0,$count,$args);
+		if (!$this->trans) {
+			$this->begin();
+			$auto=TRUE;
+		}
+	}
+	else {
+		$count=1;
+		$cmds=[$cmds];
+		$args=[$args];
+	}
+	if ($this->log===FALSE)
+		$log=FALSE;
+	//$fw=\Base::instance();
+	//$cache=\Cache::instance();
+	$result=FALSE;
+	for ($i=0;$i<$count;$i++) {
+		$cmd=$cmds[$i];
+		$arg=$args[$i];
+		// ensure 1-based arguments
+		if (array_key_exists(0,$arg)) {
+			array_unshift($arg,'');
+			unset($arg[0]);
+		}
+		if (!preg_replace('/(^\s+|[\s;]+$)/','',$cmd))
+			continue;
+		$now=microtime(TRUE);
+		$keys=$vals=[];
+		//if ($this->CACHE && $ttl && ($cached=$cache->exists(
+		if (self::CACHE && $ttl && ($cached=$cache->exists(
+			$hash=$this->hash($this->dsn.$cmd.
+			$this->stringify($arg)).($tag?'.'.$tag:'').'.sql',$result)) &&
+			$cached[0]+$ttl>microtime(TRUE)) {
+			foreach ($arg as $key=>$val) {
+				$vals[]=$this->stringify(is_array($val)?$val[0]:$val);
+				$keys[]='/'.preg_quote(is_numeric($key)?chr(0).'?':$key).
+					'/';
+			}
+		/*if ($fw->CACHE && $ttl && ($cached=$cache->exists(
+			$hash=$fw->hash($this->dsn.$cmd.
+			$fw->stringify($arg)).($tag?'.'.$tag:'').'.sql',$result)) &&
+			$cached[0]+$ttl>microtime(TRUE)) {
+			foreach ($arg as $key=>$val) {
+				$vals[]=$fw->stringify(is_array($val)?$val[0]:$val);
+				$keys[]='/'.preg_quote(is_numeric($key)?chr(0).'?':$key).
+					'/';
+			}*/
+			if ($log)
+				$this->log.=($stamp?(date('r').' '):'').'('.
+					sprintf('%.1f',1e3*(microtime(TRUE)-$now)).'ms) '.
+					'[CACHED] '.
+					preg_replace($keys,$vals,
+						str_replace('?',chr(0).'?',$cmd),1).PHP_EOL;
+		}
+		elseif (is_object($query=$this->pdo->prepare($cmd))) {
+			foreach ($arg as $key=>$val) {
+				if (is_array($val)) {
+					// User-specified data type
+					$query->bindvalue($key,$val[0],
+						$val[1]==self::PARAM_FLOAT?\PDO::PARAM_STR:$val[1]);
+					$vals[]=$this->stringify($this->value($val[1],$val[0]));
+				}
+				else {
+					// Convert to PDO data type
+					$query->bindvalue($key,$val,
+						($type=$this->type($val))==self::PARAM_FLOAT?
+							\PDO::PARAM_STR:$type);
+					$vals[]=$this->stringify($this->value($type,$val));
+				}
+				$keys[]='/'.preg_quote(is_numeric($key)?chr(0).'?':$key).
+					'/';
+			}
+			if ($log)
+				$this->log.=($stamp?(date('r').' '):'').'('.
+					sprintf('%.1f',1e3*(microtime(TRUE)-$now)).'ms) '.
+					preg_replace($keys,$vals,
+						str_replace('?',chr(0).'?',$cmd),1).PHP_EOL;
+			$query->execute();
+			$error=$query->errorinfo();
+			if ($error[0]!=\PDO::ERR_NONE) {
+				// Statement-level error occurred
+				if ($this->trans)
+					$this->rollback();
+				user_error('PDOStatement: '.$error[2],E_USER_ERROR);
+			}
+			if (preg_match('/(?:^[\s\(]*'.
+				'(?:EXPLAIN|SELECT|PRAGMA|SHOW)|RETURNING)\b/is',$cmd) ||
+				(preg_match('/^\s*(?:CALL|EXEC)\b/is',$cmd) &&
+					$query->columnCount())) {
+				$result=$query->fetchColumn();
+				// Work around SQLite quote bug
+				if (preg_match('/sqlite2?/',$this->engine))
+					foreach ($result as $pos=>$rec) {
+						unset($result[$pos]);
+						$result[$pos]=[];
+						foreach ($rec as $key=>$val)
+							$result[$pos][trim($key,'\'"[]`')]=$val;
+					}
+				$this->rows=count($result);
+				//if ($this->CACHE && $ttl)
+				if (self::CACHE && $ttl)
+					// Save to cache backend
+					$cache->set($hash,$result,$ttl);
+			}
+			else
+				$this->rows=$result=$query->rowcount();
+			$query->closecursor();
+			unset($query);
+		}
+		else {
+			$error=$this->errorinfo();
+			if ($error[0]!=\PDO::ERR_NONE) {
+				// PDO-level error occurred
+				if ($this->trans)
+					$this->rollback();
+				user_error('PDO: '.$error[2],E_USER_ERROR);
+			}
+		}
+	}
+	if ($this->trans && $auto)
+		$this->commit();
+	return $result;
+}
+//get column
+function column($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
+	$tag='';
+	if (is_array($ttl))
+		list($ttl,$tag)=$ttl;
+	$auto=FALSE;
+	if (is_null($args))
+		$args=[];
+	elseif (is_scalar($args))
+		$args=[1=>$args];
+	if (is_array($cmds)) {
+		if (count($args)<($count=count($cmds)))
+			// Apply arguments to SQL commands
+			$args=array_fill(0,$count,$args);
+		if (!$this->trans) {
+			$this->begin();
+			$auto=TRUE;
+		}
+	}
+	else {
+		$count=1;
+		$cmds=[$cmds];
+		$args=[$args];
+	}
+	if ($this->log===FALSE)
+		$log=FALSE;
+	//$fw=\Base::instance();
+	//$cache=\Cache::instance();
+	$result=FALSE;
+	for ($i=0;$i<$count;$i++) {
+		$cmd=$cmds[$i];
+		$arg=$args[$i];
+		// ensure 1-based arguments
+		if (array_key_exists(0,$arg)) {
+			array_unshift($arg,'');
+			unset($arg[0]);
+		}
+		if (!preg_replace('/(^\s+|[\s;]+$)/','',$cmd))
+			continue;
+		$now=microtime(TRUE);
+		$keys=$vals=[];
+		//if ($this->CACHE && $ttl && ($cached=$cache->exists(
+		if (self::CACHE && $ttl && ($cached=$cache->exists(
+			$hash=$this->hash($this->dsn.$cmd.
+			$this->stringify($arg)).($tag?'.'.$tag:'').'.sql',$result)) &&
+			$cached[0]+$ttl>microtime(TRUE)) {
+			foreach ($arg as $key=>$val) {
+				$vals[]=$this->stringify(is_array($val)?$val[0]:$val);
+				$keys[]='/'.preg_quote(is_numeric($key)?chr(0).'?':$key).
+					'/';
+			}
+		/*if ($fw->CACHE && $ttl && ($cached=$cache->exists(
+			$hash=$fw->hash($this->dsn.$cmd.
+			$fw->stringify($arg)).($tag?'.'.$tag:'').'.sql',$result)) &&
+			$cached[0]+$ttl>microtime(TRUE)) {
+			foreach ($arg as $key=>$val) {
+				$vals[]=$fw->stringify(is_array($val)?$val[0]:$val);
+				$keys[]='/'.preg_quote(is_numeric($key)?chr(0).'?':$key).
+					'/';
+			}*/
+			if ($log)
+				$this->log.=($stamp?(date('r').' '):'').'('.
+					sprintf('%.1f',1e3*(microtime(TRUE)-$now)).'ms) '.
+					'[CACHED] '.
+					preg_replace($keys,$vals,
+						str_replace('?',chr(0).'?',$cmd),1).PHP_EOL;
+		}
+		elseif (is_object($query=$this->pdo->prepare($cmd))) {
+			foreach ($arg as $key=>$val) {
+				if (is_array($val)) {
+					// User-specified data type
+					$query->bindvalue($key,$val[0],
+						$val[1]==self::PARAM_FLOAT?\PDO::PARAM_STR:$val[1]);
+					$vals[]=$this->stringify($this->value($val[1],$val[0]));
+				}
+				else {
+					// Convert to PDO data type
+					$query->bindvalue($key,$val,
+						($type=$this->type($val))==self::PARAM_FLOAT?
+							\PDO::PARAM_STR:$type);
+					$vals[]=$this->stringify($this->value($type,$val));
+				}
+				$keys[]='/'.preg_quote(is_numeric($key)?chr(0).'?':$key).
+					'/';
+			}
+			if ($log)
+				$this->log.=($stamp?(date('r').' '):'').'('.
+					sprintf('%.1f',1e3*(microtime(TRUE)-$now)).'ms) '.
+					preg_replace($keys,$vals,
+						str_replace('?',chr(0).'?',$cmd),1).PHP_EOL;
+			$query->execute();
+			$error=$query->errorinfo();
+			if ($error[0]!=\PDO::ERR_NONE) {
+				// Statement-level error occurred
+				if ($this->trans)
+					$this->rollback();
+				user_error('PDOStatement: '.$error[2],E_USER_ERROR);
+			}
+			if (preg_match('/(?:^[\s\(]*'.
+				'(?:EXPLAIN|SELECT|PRAGMA|SHOW)|RETURNING)\b/is',$cmd) ||
+				(preg_match('/^\s*(?:CALL|EXEC)\b/is',$cmd) &&
+					$query->columnCount())) {
+				$result=$query->fetchAll(PDO::FETCH_COLUMN);
+				// Work around SQLite quote bug
+				if (preg_match('/sqlite2?/',$this->engine))
+					foreach ($result as $pos=>$rec) {
+						unset($result[$pos]);
+						$result[$pos]=[];
+						foreach ($rec as $key=>$val)
+							$result[$pos][trim($key,'\'"[]`')]=$val;
+					}
+				$this->rows=count($result);
+				//if ($this->CACHE && $ttl)
+				if (self::CACHE && $ttl)
+					// Save to cache backend
+					$cache->set($hash,$result,$ttl);
+			}
+			else
+				$this->rows=$result=$query->rowcount();
+			$query->closecursor();
+			unset($query);
+		}
+		else {
+			$error=$this->errorinfo();
+			if ($error[0]!=\PDO::ERR_NONE) {
+				// PDO-level error occurred
+				if ($this->trans)
+					$this->rollback();
+				user_error('PDO: '.$error[2],E_USER_ERROR);
+			}
+		}
+	}
+	if ($this->trans && $auto)
+		$this->commit();
+	return $result;
+}
 /** custom add stop **/
 	/**
 	*	Begin SQL transaction
@@ -238,7 +497,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 		$this->trans=TRUE;
 		return $out;
 	}
-
 	/**
 	*	Rollback SQL transaction
 	*	@return bool
@@ -248,7 +506,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 		$this->trans=FALSE;
 		return $out;
 	}
-
 	/**
 	*	Commit SQL transaction
 	*	@return bool
@@ -258,7 +515,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 		$this->trans=FALSE;
 		return $out;
 	}
-
 	/**
 	*	Return transaction flag
 	*	@return bool
@@ -266,7 +522,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 	function trans() {
 		return $this->trans;
 	}
-
 	/**
 	*	Map data type of argument to a PDO constant
 	*	@return int
@@ -288,7 +543,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 				return \PDO::PARAM_STR;
 		}
 	}
-
 	/**
 	*	Cast value to PHP type
 	*	@return mixed
@@ -313,7 +567,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 				return (binary)$val;
 		}
 	}
-
 	/**
 	*	Execute SQL statement(s)
 	*	@return array|int|FALSE
@@ -458,7 +711,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 			$this->commit();
 		return $result;
 	}
-
 	/**
 	*	Return number of rows affected by last query
 	*	@return int
@@ -466,7 +718,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 	function count() {
 		return $this->rows;
 	}
-
 	/**
 	*	Return SQL profiler results (or disable logging)
 	*	@param $flag bool
@@ -477,7 +728,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 			return $this->log;
 		$this->log=FALSE;
 	}
-
 	/**
 	*	Retrieve schema of SQL table
 	*	@return array|FALSE
@@ -585,7 +835,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 		user_error(sprintf(self::E_PKey,$table),E_USER_ERROR);
 		return FALSE;
 	}
-
 	/**
 	*	Quote string
 	*	@return string
@@ -599,7 +848,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 				$val):
 			$this->pdo->quote($val,$type);
 	}
-
 	/**
 	*	Return UUID
 	*	@return string
@@ -607,7 +855,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 	function uuid() {
 		return $this->uuid;
 	}
-
 	/**
 	*	Return parent object
 	*	@return \PDO
@@ -615,7 +862,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 	function pdo() {
 		return $this->pdo;
 	}
-
 	/**
 	*	Return database engine
 	*	@return string
@@ -623,7 +869,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 	function driver() {
 		return $this->engine;
 	}
-
 	/**
 	*	Return server version
 	*	@return string
@@ -631,7 +876,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 	function version() {
 		return $this->pdo->getattribute(\PDO::ATTR_SERVER_VERSION);
 	}
-
 	/**
 	*	Return database name
 	*	@return string
@@ -639,7 +883,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 	function name() {
 		return $this->dbname;
 	}
-
 	/**
 	*	Return quoted identifier name
 	*	@return string
@@ -661,7 +904,6 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 		return $use[0].($split ? implode($use[1].'.'.$use[0],explode('.',$key))
 			: $key).$use[1];
 	}
-
 	/**
 	*	Redirect call to PDO object
 	*	@return mixed
@@ -671,11 +913,9 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 	/*function __call($func,array $args) {
 		return call_user_func_array([$this->pdo,$func],$args);
 	}*/
-
 	//! Prohibit cloning
 	private function __clone() {
 	}
-
 	/**
 	*	Instantiate class
 	*	@param $dsn string
@@ -697,5 +937,4 @@ function row($cmds,$args=NULL,$ttl=0,$log=TRUE,$stamp=FALSE) {
 		$this->pdo=new \PDO($dsn,$user,$pw,$options);
 		$this->engine=$this->pdo->getattribute(\PDO::ATTR_DRIVER_NAME);
 	}
-
 }
